88490ccdcb3cd991c31a64b715a32329afdce5aa
lbdeyo <lbdeyo@gmail.com>
2025-08-16 11:12:29 -0500

    tightening up the margins on ed materials

--- app/educational-materials/page.tsx
+++ app/educational-materials/page.tsx
@@ -2,8 +2,8 @@ export default function EducationalMaterials() {
     return (
         <div className="bg-[url('/img/art/ed-materials-bg.jpg')] md:bg-cover ">
 
-            <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 pb-12 pt-12 ">
-                <div className="max-w-3xl p-7 bg-[url('/img/art/paper-light.jpg')] bg-top bg-repeat bg-[length:1000px_auto] shadow-lg shadow-black">
+            <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 pb-12 pt-5 ">
+                <div className="max-w-3xl p-5 bg-[url('/img/art/paper-light.jpg')] bg-top bg-repeat bg-[length:1000px_auto] shadow-lg shadow-black">
                     <div className="space-y-8">
                         {/* Page Header */}
                         <div className="max-w-3xl mt-8">
@@ -33,7 +33,7 @@ export default function EducationalMaterials() {
                         </div>
 
                         {/* Complete Learning Guide */}
-                        <div className="max-w-3xl m-3  bg-opacity-100 p-7 border border-6 border-black" style={{
+                        <div className="max-w-3xl   bg-opacity-100 p-7 border border-6 border-black" style={{
                             backgroundColor: '#D4D6AD'
                         }}>                        <h2 className="text-4xl font-bold mb-6">COMPLETE LEARNING GUIDE</h2>
 
