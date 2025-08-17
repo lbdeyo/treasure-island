eff83ca0b43210bb98a1297af0ba9d8ec1cb046b
lbdeyo <lbdeyo@gmail.com>
2025-08-17 14:38:07 -0500

    Many styles

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,60 +2,62 @@ import Image from "next/image";
 
 export default function About() {
     return (
-        <div className="bg-[url('/img/art/paper-light.jpg')] bg-cover bg-top bg-repeat">
-            <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+        <div className="bg-[url('/img/art/p-23-parchment.jpg')] bg-cover bg-top bg-repeat">
+            <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
                 <div className="space-y-8">
                     {/* Page Header */}
-                    <div className="max-w-3xl">
-                        <h1 className=" mb-8">About the Play</h1>
-                    </div>
+                    <div className="relative max-w-3xl p-8">
+                        <div aria-hidden className="absolute inset-0 z-0 pointer-events-none bg-[url('/img/torn-paper-mask.png')] [background-size:100%_100%] bg-no-repeat opacity-50 [filter:blur(28px)_brightness(0.2)] transform translate-y-[5px] scale-[1.015]"></div>
+                        <div aria-hidden className="absolute inset-0 z-10 pointer-events-none bg-[url('/img/art/paper-light.jpg')] bg-top bg-repeat bg-[length:1000px_auto] [mask-image:url('/img/torn-paper-mask.png')] [mask-size:100%_100%] [-webkit-mask-image:url('/img/torn-paper-mask.png')] [-webkit-mask-size:100%_100%] [mask-repeat:no-repeat] [-webkit-mask-repeat:no-repeat]"></div>
+                        <div className="relative z-20">
+                            <h1 className=" mb-8 pt-8">About the Play</h1>
+
+                            {/* Main Description */}
+                            <div className="flex flex-col lg:flex-row lg:gap-12 lg:items-start">
+                                <div className="max-w-3xl flex-1">
+                                    <p className="text-lg leading-relaxed mb-6">
+                                        When his daughter Scarlett was nine years old, Jason Neulander read <i>Treasure Island</i> aloud to her. He was surprised to discover that he was as caught up in the story as she was. Flash forward several years.</p>
+
+                                    <p className="text-lg leading-relaxed mb-6">Summer, 2024, Jason and Simon Shaw were biking together in the Berkshires and Simon asked Jason if he'd ever consider putting a new live show together along the lines of <i>The Intergalactic Nemesis</i>. The timing of the discussion was perfect because Jason was looking for his next project. His immediate thought: adapt <i>Treasure Island</i>.</p>
+                                    <Image
+                                        src="/img/art/ghost-1.png"
+                                        alt="Paper light"
+                                        width={400}
+                                        height={500}
+                                        className="object-contain float-right"
+                                    />
+                                    <p className="text-lg leading-relaxed mb-6">But Jason wanted the show to be different from <i>The Intergalactic Nemesis</i> in a few ways. First, he wanted the art to have a decidedly retro look. Second, he wanted the performers to not only voice all the character, but also, and simultaneously, perform the sound effects and music.</p>
 
 
-                    {/* Main Description */}
-                    <div className="flex flex-col lg:flex-row lg:gap-12 lg:items-start">
-                        <div className="max-w-3xl flex-1">
-                            <p className="text-lg leading-relaxed mb-6">
-                                When his daughter Scarlett was nine years old, Jason Neulander read <i>Treasure Island</i> aloud to her. He was surprised to discover that he was as caught up in the story as she was. Flash forward several years.</p>
-
-                            <p className="text-lg leading-relaxed mb-6">Summer, 2024, Jason and Simon Shaw were biking together in the Berkshires and Simon asked Jason if he'd ever consider putting a new live show together along the lines of <i>The Intergalactic Nemesis</i>. The timing of the discussion was perfect because Jason was looking for his next project. His immediate thought: adapt <i>Treasure Island</i>.</p>
-                            <Image
-                                src="/img/art/ghost-1.png"
-                                alt="Paper light"
-                                width={400}
-                                height={500}
-                                className="object-contain float-right"
-                            />
-                            <p className="text-lg leading-relaxed mb-6">But Jason wanted the show to be different from <i>The Intergalactic Nemesis</i> in a few ways. First, he wanted the art to have a decidedly retro look. Second, he wanted the performers to not only voice all the character, but also, and simultaneously, perform the sound effects and music.</p>
 
 
+                                    <p className="text-lg leading-relaxed mb-6">At a dinner with his friend Jeff Pinsker, Jason mentioned the new project. Jeff immediately contacted Rob Jones at Mondo who put Jason in touch with Johnny Dombrowski. Jason took one look at Johnny's Instagram feed and knew this was the guy.</p>
 
+                                    <p className="text-lg leading-relaxed mb-6">With an artist secured, Jason contacted his old friend Sam Lipman to ask if he could compose the music. Sam said yes immediately. Jason then asked Buzz Moran, the genius behind the sound effects for <i>The Intergalactic Nemesis</i> , if he'd come on board as sound designer. A few months later, Lee Duhig joined as colorist and David Hutchison as letterer for the book version.</p>
+                                    {/* Ghost Image */}
+                                    <div className="lg:flex-shrink-0 mt-8 lg:mt-0 flex justify-center lg:justify-start float-left">
+                                        <Image
+                                            src="/img/art/ghost-2.png"
+                                            alt="Ghost illustration"
+                                            width={400}
+                                            height={500}
+                                            className="object-contain "
+                                        />
+                                    </div>
+                                    <p className="text-lg leading-relaxed mb-6">The only thing left to do was secure a premiere. Jason called the programmer for Austin's Paramount Theater, Lietza Brass, and before he could finish describing the show, she committed to a date. September 26, 2026.</p>
 
-                            <p className="text-lg leading-relaxed mb-6">At a dinner with his friend Jeff Pinsker, Jason mentioned the new project. Jeff immediately contacted Rob Jones at Mondo who put Jason in touch with Johnny Dombrowski. Jason took one look at Johnny's Instagram feed and knew this was the guy.</p>
+                                    <p className="text-lg leading-relaxed mb-6">A show was born.</p>
 
-                            <p className="text-lg leading-relaxed mb-6">With an artist secured, Jason contacted his old friend Sam Lipman to ask if he could compose the music. Sam said yes immediately. Jason then asked Buzz Moran, the genius behind the sound effects for <i>The Intergalactic Nemesis</i> , if he'd come on board as sound designer. A few months later, Lee Duhig joined as colorist and David Hutchison as letterer for the book version.</p>
-                            {/* Ghost Image */}
-                            <div className="lg:flex-shrink-0 mt-8 lg:mt-0 flex justify-center lg:justify-start float-left">
-                                <Image
-                                    src="/img/art/ghost-2.png"
-                                    alt="Ghost illustration"
-                                    width={400}
-                                    height={500}
-                                    className="object-contain "
-                                />
+                                    <p className="text-lg leading-relaxed mb-6">The purpose of this website is to serve both venues who might book the tour and audience members who might want to take a deeper dive. Don't hesitate to explore! And by all means, contact us if you have any questions or simply want to get in touch.</p>
+
+                                    <p className="text-lg leading-relaxed mb-12">See you at the theater.</p>
+                                </div>
+
                             </div>
-                            <p className="text-lg leading-relaxed mb-6">The only thing left to do was secure a premiere. Jason called the programmer for Austin's Paramount Theater, Lietza Brass, and before he could finish describing the show, she committed to a date. September 26, 2026.</p>
-
-                            <p className="text-lg leading-relaxed mb-6">A show was born.</p>
-
-                            <p className="text-lg leading-relaxed mb-6">The purpose of this website is to serve both venues who might book the tour and audience members who might want to take a deeper dive. Don't hesitate to explore! And by all means, contact us if you have any questions or simply want to get in touch.</p>
-
-                            <p className="text-lg leading-relaxed mb-6">See you at the theater.</p>
                         </div>
-
-
                     </div>
-                </div>
-            </div >
+                </div >
+            </div>
         </div>
     );
 }
--- app/sample-video/page.tsx
+++ app/sample-video/page.tsx
@@ -1,23 +1,38 @@
 export default function SampleVideo() {
     return (
-        <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
-            <div className="space-y-8">
-                {/* Page Header */}
-                <div className="max-w-3xl">
-                    <h1 className=" mb-8" style={{ fontSize: '5rem' }}>Sample Video</h1>
-                </div>
+        <div className="bg-[url('/img/art/p19-insert.jpg')] bg-cover bg-top bg-repeat min-h-screen">
 
-                {/* Coming Soon */}
-                <div className="max-w-3xl">
-                    <div style={{ padding: '56.25% 0 0 0', position: 'relative' }}>
-                        <iframe
-                            src="https://player.vimeo.com/video/1108447276?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
-                            frameBorder="0"
-                            allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share"
-                            referrerPolicy="strict-origin-when-cross-origin"
-                            style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
-                            title="Treasure Island Reimagined - demo video"
-                        />
+            <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+                <div className="space-y-8">
+                    {/* Page Header */}
+                    <div className="relative max-w-3xl p-8">
+                        <div aria-hidden className="absolute inset-0 z-0 pointer-events-none bg-[url('/img/torn-paper-mask.png')] [background-size:100%_100%] bg-no-repeat opacity-50 [filter:blur(28px)_brightness(0.2)] transform translate-y-[5px] scale-[1.015]"></div>
+                        <div aria-hidden className="absolute inset-0 z-10 pointer-events-none bg-[url('/img/art/paper-light.jpg')] bg-top bg-repeat bg-[length:1000px_auto] [mask-image:url('/img/torn-paper-mask.png')] [mask-size:100%_100%] [-webkit-mask-image:url('/img/torn-paper-mask.png')] [-webkit-mask-size:100%_100%] [mask-repeat:no-repeat] [-webkit-mask-repeat:no-repeat]"></div>
+                        <div className="relative z-20">
+                            <h1 className=" pt-5 " style={{ fontSize: '5rem' }}>Sample Video</h1>
+                            <p className="text-2xl text-black pt-3 pb-5">
+                                Password: Janey
+                            </p>
+
+                            {/* Coming Soon */}
+
+                            <div style={{ padding: '56.25% 0 0 0', position: 'relative' }}>
+                                <iframe
+                                    src="https://player.vimeo.com/video/1108447276?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+                                    frameBorder="0"
+                                    allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share"
+                                    referrerPolicy="strict-origin-when-cross-origin"
+                                    style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
+                                    title="Treasure Island Reimagined - demo video"
+                                />
+                            </div>
+                            <p className="text-xl text-center text-black p-5">
+                                Matthew Armbruster -  voice and cello •
+                                Danu Mara - voices and sound effects<br />
+                                Jason Neulander - voices and keyboards •
+                                Marc Pouhé - voices and sound effects<br />
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
